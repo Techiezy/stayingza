e6fa352571bd08c117aeeec21ea580398052beb3
tanu70998 <tanu70998@gmail.com>
2022-10-16 03:23:47 +0530

    pro init

---
+++ src/main/java/com/tanupro/app/stayingza/StayingzaApplication.java
@@ -0,0 +1,13 @@
+package com.tanupro.app.stayingza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayingzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayingzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanupro/app/stayingza/StayingzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanupro.app.stayingza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayingzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
