f5ff9cd1b3874c2e1c3054f0aea660e5db37e332
tanu70998 <tanu70998@gmail.com>
2022-10-16 05:04:50 +0530

    Additional Repository <Guest,Reservation>

---
+++ src/main/java/com/tanupro/app/stayingza/data/ReservationRepository.java
@@ -0,0 +1,11 @@
+package com.tanupro.app.stayingza.data;
+
+import java.sql.Date;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    Iterable<Reservation> findReservationByReservationDate(Date date);
+}
--- src/main/java/com/tanupro/app/stayingza/util/AppStartupEvent.java
+++ src/main/java/com/tanupro/app/stayingza/util/AppStartupEvent.java
@@ -1,5 +1,9 @@
 package com.tanupro.app.stayingza.util;
 
+import com.tanupro.app.stayingza.data.Guest;
+import com.tanupro.app.stayingza.data.GuestRepository;
+import com.tanupro.app.stayingza.data.Reservation;
+import com.tanupro.app.stayingza.data.ReservationRepository;
 import com.tanupro.app.stayingza.data.Room;
 import com.tanupro.app.stayingza.data.RoomRepository;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -9,14 +13,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class AppStartupEvent implements ApplicationListener<ApplicationReadyEvent> {
     private final RoomRepository roomRepository;
+    private final GuestRepository guestRepository;
+    private final ReservationRepository reservationRepository;
 
-    public AppStartupEvent(RoomRepository roomRepository) {
+    public AppStartupEvent(RoomRepository roomRepository,GuestRepository guestRepository,ReservationRepository reservationRepository) {
         this.roomRepository = roomRepository;
+        this.guestRepository = guestRepository;
+        this.reservationRepository = reservationRepository;
     }
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
         Iterable<Room> rooms = this.roomRepository.findAll();
         rooms.forEach(System.out::println);
+        Iterable<Guest> guests = this.guestRepository.findAll();
+        guests.forEach(System.out::println);
+        Iterable<Reservation> reservations = this.reservationRepository.findAll();
+        reservations.forEach(System.out::println);
     }
 }
