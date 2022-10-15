883df97535184b80fd031767077c5befd1845506
tanu70998 <tanu70998@gmail.com>
2022-10-16 04:49:00 +0530

    RoomRepository from h2database

---
+++ src/main/java/com/tanupro/app/stayingza/util/AppStartupEvent.java
@@ -0,0 +1,22 @@
+package com.tanupro.app.stayingza.util;
+
+import com.tanupro.app.stayingza.data.Room;
+import com.tanupro.app.stayingza.data.RoomRepository;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppStartupEvent implements ApplicationListener<ApplicationReadyEvent> {
+    private final RoomRepository roomRepository;
+
+    public AppStartupEvent(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        Iterable<Room> rooms = this.roomRepository.findAll();
+        rooms.forEach(System.out::println);
+    }
+}
